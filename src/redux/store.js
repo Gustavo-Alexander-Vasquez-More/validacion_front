494547db2e0efc7fc8b491a6fe8875fd7adf65b8
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-09-08 18:30:32 -0300

    actualizacion

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import estadosReducer from "./reducers/reducer.js";
 import adminsReducer from './reducers/adminsReducer.js'
-
+import licenciasReducer from "./reducers/licenciaReducer.js";
 export const store = configureStore({
-    reducer: {
-        estados:estadosReducer,
-        admins:adminsReducer
-      },
+reducer: {
+    estados:estadosReducer,
+    admins:adminsReducer,
+    licencias:licenciasReducer
+},
 })
