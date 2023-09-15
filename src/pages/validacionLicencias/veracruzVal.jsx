b66e22c7c1ae16875bcdc3361ce84ca90debf399
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-09-15 19:52:15 -0300

    correcion 2

--- src/pages/validacionLicencias/veracruzVal.jsx
+++ src/pages/validacionLicencias/veracruzVal.jsx
@@ -41,37 +41,37 @@ export default function veracruzVal() {
     return `${dia.toString().padStart(2, '0')}-${mes.toString().padStart(2, '0')}-${anio}`;
   }
   return (
-    <div className='w-full h-screen  px-[8rem]'>
+    <div className='w-full h-screen  sm:px-[8rem] flex flex-col items-center sm:block'>
     <div className=' w-full h-[15vh] flex justify-center items-end'>
     <img className='lg:w-[15rem] sm:w-[10rem]  h-[5rem]' src="https://firebasestorage.googleapis.com/v0/b/validacion-de-licencias-c813d.appspot.com/o/Veracruz%2Flogo.png?alt=media&token=e1e00656-3ec9-49d9-9b9a-4bb3ffaec8d7" alt="" />
     </div>
     <div className='w-full h-[90vh] flex'>
-    <div className=' lg:w-[60%] lg:h-[90vh] flex items-end lg:py-[2rem] sm:py-[1.5rem] bg-[white] '>
-    <div className='absolute lg:w-[25%] lg:h-[80vh] sm:right-[55%] sm:h-[80vh]  lg:right-[60%] border-solid border-[1px] border-[#c7c4c4] bg-[white] animate-rotate-x'>
+    <div className='lg:w-[60%] lg:h-[90vh] flex sm:items-end lg:py-[2rem] sm:py-[1.5rem]  w-full justify-center items-center'>
+    <div className='h-[70vh] absolute lg:w-[25%] lg:h-[80vh] sm:right-[55%] sm:h-[80vh]  lg:right-[60%] border-solid border-[1px] border-[#c7c4c4] bg-[white] animate-rotate-x rounded-[5px]'>
     <div className='w-full h-[5vh]  text-[#8d2f3b] flex justify-center items-center text-[1.2rem] border-solid border-[1px] border-[#c7c4c4]'>Datos personales</div>
     <div className='w-full h-[15vh] flex justify-center py-[1rem]'>
         <img className='h-[13vh] w-[6rem]' src={licenciaEncontrada ? licenciaEncontrada.foto : 'nothing' } alt="" />
     </div>
     <div className='w-full h-[49vh] px-[1.5rem] flex flex-col gap-1 py-[0.5rem]'>
-    <p className='text-[#8d2f3b]'>NOMBRE COMPLETO</p>
+    <p className='text-[#8d2f3b] sm:text-[1rem] text-[0.8rem]'>NOMBRE COMPLETO</p>
     <div className='border-[1px] border-solid border-[#c7c4c4] rounded-[5px] h-[2.5rem] flex items-center px-[1rem]'><p className='text-[#000000]'>{licenciaEncontrada ? licenciaEncontrada.nombre : 'Nombre no encontrado'}</p>
 </div>
-    <p className='text-[#8d2f3b]'>FOLIO</p>
+    <p className='text-[#8d2f3b] sm:text-[1rem] text-[0.8rem]'>FOLIO</p>
     <div className='border-[1px] border-solid border-[#c7c4c4] rounded-[5px] h-[2.5rem] flex items-center px-[1rem]'><p className='text-[#000000]'>{licenciaEncontrada ? licenciaEncontrada.folio : 'Nombre no encontrado'}</p></div>
-    <p className='text-[#8d2f3b]'>TIPO</p>
+    <p className='text-[#8d2f3b] sm:text-[1rem] text-[0.8rem]'>TIPO</p>
     <div className='border-[1px] border-solid border-[#c7c4c4] rounded-[5px] h-[2.5rem] flex items-center px-[1rem]'><p className='text-[#000000]'>{licenciaEncontrada ? licenciaEncontrada.tipo : 'Tipo no encontrado'}</p></div>
-    <p className='text-[#8d2f3b]'>RFC / CURP</p>
+    <p className='text-[#8d2f3b] sm:text-[1rem] text-[0.8rem]'>RFC / CURP</p>
     <div className='border-[1px] border-solid border-[#c7c4c4] rounded-[5px] h-[2.5rem] flex items-center px-[1rem]'><p className='text-[#000000]'>{licenciaEncontrada ? licenciaEncontrada.rfc_curp : 'Folio no encontrado'}</p></div>
-    <p className='text-[#8d2f3b]'>FECHA DE EXPEDICIÓN</p>
+    <p className='text-[#8d2f3b] sm:text-[1rem] text-[0.8rem]'>FECHA DE EXPEDICIÓN</p>
     <div className='border-[1px] border-solid border-[#c7c4c4] rounded-[5px] h-[2.5rem] flex items-center px-[1rem]'><p className='text-[#000000]'>{licenciaEncontrada ? formatearFecha(licenciaEncontrada.expedicion) : 'Fecha no encontrada'}</p></div>
-    <p className='text-[#8d2f3b]'>VIGENCIA</p>
+    <p className='text-[#8d2f3b] sm:text-[1rem] text-[0.8rem]'>VIGENCIA</p>
     <div className='border-[1px] border-solid border-[#c7c4c4] rounded-[5px] h-[2.5rem] flex items-center px-[1rem]'><p className='text-[#000000]'>{licenciaEncontrada ? formatearFecha(licenciaEncontrada.vigencia) : 'Fecha no encontrada'}</p></div>
     </div>
     </div>
     <div className='bg-[url("https://firebasestorage.googleapis.com/v0/b/validacion-de-licencias-c813d.appspot.com/o/Veracruz%2F2.png?alt=media&token=f721bb25-c3be-420b-80a8-04f42f2d371d")] w-full h-[30vh] bg-contain bg-no-repeat'></div>
     </div>
-    <div className='w-[45%] h-[80vh]'></div>
-    <div className='absolute lg:w-[40%] sm:w-[35%] lg:h-[30vh] sm:h-[29vh] bg-[white] lg:left-[50%] lg:bottom-[50%] sm:left-[50%] sm:bottom-[50%] lg:px-[3rem] sm:px-[2rem] border-[1px] border-solid border-[#c7c4c4] font-semibold flex flex-col justify-around py-[2rem] items-center text-center animate-rotate-x rounded-[5px]'>
+    
+    <div className='hidden absolute lg:w-[40%] sm:w-[35%] lg:h-[30vh] sm:h-[29vh] bg-[white] lg:left-[50%] lg:bottom-[50%] sm:left-[50%] sm:bottom-[50%] lg:px-[3rem] sm:px-[2rem] border-[1px] border-solid border-[#c7c4c4] font-semibold sm:flex flex-col justify-around py-[2rem] items-center text-center animate-rotate-x rounded-[5px]'>
     <p className='text-[#8d2f3b] lg:text-[1.5rem]'>Validación de licencias de conducir</p>
     <img className='w-[15rem]' src="https://firebasestorage.googleapis.com/v0/b/validacion-de-licencias-c813d.appspot.com/o/Veracruz%2Flogo.png?alt=media&token=e1e00656-3ec9-49d9-9b9a-4bb3ffaec8d7" alt="" />
     </div>
